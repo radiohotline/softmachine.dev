49b7e09d4b447959a4112c27c47a0455cde5f33e
Radiohotline <radiohotline@disroot.org>
2026-04-24 10:58:13 +0000

    webring

--- webring/linuxring/webring.py
+++ webring/linuxring/webring.py
@@ -1,20 +1,24 @@
-url = input("URL: ")
-name = url.replace("https:", "").replace("/","")
-desc = input("Desc: ")
-icon = []
-icon_no = int(input("How many icons? "))
-count = 0
+num = int(input("How many? "))
+i = 0
+while i < num:
+	url = input("URL: ")
+	name = url.replace("https:", "").replace("/","")
+	desc = input("Desc: ")
+	icon = []
+	icon_no = int(input("How many icons? "))
+	count = 0
 
-while (count < icon_no):
-    icon_in = input("Icon {}: ".format(count+1)) 
-    icon.append(icon_in)
-    count+=1
+	while (count < icon_no):
+	    icon_in = input("Icon {}: ".format(count+1)) 
+	    icon.append(icon_in)
+	    count+=1
 
-print("<tr>")
-print("\t<td>", end='')
-for item in icon:
-    print('<img class="icon" src="icon/', item, '.png">', sep='', end='')
-print('</td>')
-print('\t<td><a href="', url, '">', name, '</a></td>', sep='')
-print('\t<td>', desc, '</td>', sep='')
-print("</tr>")
+	print("<tr>")
+	print("\t<td>", end='')
+	for item in icon:
+    		print('<img class="icon" src="icon/', item, '.png">', sep='', end='')
+	print('</td>')
+	print('\t<td><a href="', url, '">', name, '</a></td>', sep='')
+	print('\t<td>', desc, '</td>', sep='')
+	print("</tr>")
+	i+=1
